3f4b31637f46080a8872d0be616e922f08afdbb7
Rawin Deeboonchai <comtalyst@outlook.com>
2017-05-03 21:18:02 +0700

    Implement global hotkeys for "key shortcuting"
    - Use JIntelitype API for global hotkeys
    - Use java.awt.Robot to simulate shortcuted keys
    Example : We press "Win+Alt+S" --> Result is "AAAAA"
    - Use Multithreading for Holding Keys
    - Hotkey queries won't be stack or cancel right now (It will be fixed
    soon)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,46 +1,100 @@
 package sample;
 
+import com.melloware.jintellitype.HotkeyListener;
 import com.melloware.jintellitype.JIntellitype;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Main extends Application {
-    boolean status=false;
+    private Robot robot;
+    private boolean status=false,running=false;
+    static int holdSpeed=30;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        // JavaFX Scene Process
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("KeyCheater");
         Scene scene = new Scene(root, 150, 100);
-        
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                KeyCode key = event.getCode();
-                if(key == KeyCode.F11){
-                    status = true;
-                }
-                else if(key == KeyCode.F10){
-                    System.out.println(Boolean.toString(status));
-                }
-                else if(key == KeyCode.F12){
-                    status = false;
-                }
-            }
-        });
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        // Variable Declaration/Initialization
+        robot = new Robot();
+
+        // Set Base Hotkeys
+        JIntellitype.getInstance().registerHotKey(-3, JIntellitype.MOD_CONTROL + JIntellitype.MOD_WIN, (int)'Z');   // Terminate Listener (Ctrl+Win+Z)
+        JIntellitype.getInstance().registerHotKey(-1, JIntellitype.MOD_WIN + JIntellitype.MOD_ALT, (int)'Z');       // Start Listener (Win+Alt+Z)
+
+        // Setup Listener
+        JIntellitype.getInstance().addHotKeyListener(new HotkeyListener(){
+            @Override
+            public void onHotKey(int id) {
+                if(id == -3){               // Terminate Listener
+                    status = false;
+                    JIntellitype.getInstance().cleanUp();
+                    System.out.println("Listening Terminated");
+                    System.exit(0);
+                }
+                else if(id == -1){          // Start Listener
+                    status = true;
+                    JIntellitype.getInstance().unregisterHotKey(-1);
+                    JIntellitype.getInstance().registerHotKey(-2, JIntellitype.MOD_WIN + JIntellitype.MOD_ALT, (int)'Z');
+                    regAll();               // Register all hotkeys
+                    System.out.println("Listening Started");
+                }
+                else if(id == -2){          // Pause Listener
+                    status = false;
+                    JIntellitype.getInstance().unregisterHotKey(-2);
+                    JIntellitype.getInstance().registerHotKey(-1, JIntellitype.MOD_WIN + JIntellitype.MOD_ALT, (int)'Z');
+                    unregAll();             // Unregister all hotkeys
+                    System.out.println("Listening Paused");
+                }
+
+                else if(status){            // Ready to execute keys
+                    unregAll();             // Temporarily Unregister all hotkeys (to prevent key stacking)
+
+                    if(id == 999){
+                        running = true;
+                        Thread t = new Thread(new Runnable(){       // Create Executor Thread
+                            public void run() {
+                                while(running) {
+                                    robot.keyPress(KeyEvent.VK_A);
+                                    try{
+                                        Thread.sleep(holdSpeed);
+                                    }
+                                    catch (Exception e){ e.printStackTrace(); }
+                                }
+                                robot.keyRelease(KeyEvent.VK_A);
+                            }
+                        });
+                        t.start();
+                        try{
+                            Thread.sleep(2000);
+                        } catch(Exception e){ e.printStackTrace(); }
+                        running = false;
+                    }
+
+                    regAll();
+                }
+            }
+        });
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    private void regAll(){
+        // Hotkeys here
+        JIntellitype.getInstance().registerHotKey(999, JIntellitype.MOD_WIN + JIntellitype.MOD_ALT, (int)'S');  // Sample : Hold 'A' for 2 seconds (Win+Alt+S)
     }
+    private void unregAll(){
+        JIntellitype.getInstance().unregisterHotKey(999);
+    }
+
+    public static void main(String[] args) { launch(args); }
 }

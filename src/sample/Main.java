bf70b46b847f9b192e1b2eecf2ff6b84e26479fe
Rawin Deeboonchai <comtalyst@outlook.com>
2017-05-03 15:30:43 +0700

    Add basic hotkeys function
    - You can use hotkeys while window is open
    - Add JIntelitype API for global hotkeys (later)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,42 @@
 package sample;
 
+import com.melloware.jintellitype.JIntellitype;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    boolean status=false;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("KeyCheater");
+        Scene scene = new Scene(root, 150, 100);
+        
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                KeyCode key = event.getCode();
+                if(key == KeyCode.F11){
+                    status = true;
+                }
+                else if(key == KeyCode.F10){
+                    System.out.println(Boolean.toString(status));
+                }
+                else if(key == KeyCode.F12){
+                    status = false;
+                }
+            }
+        });
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
